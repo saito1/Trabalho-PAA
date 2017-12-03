cd2aa0f5c6e30df05956f258efff782fc9fb8791
marianacavichioli <cavichiolimariana@gmail.com>
2017-12-03 19:42:02 -0200

    Prim e Kruskal comentados.

--- Kruskal.py
+++ Kruskal.py
@@ -1,70 +1,80 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+#Importando as bibliotexas do Python.
 import networkx as nx
 import numpy as n
 import matplotlib.pyplot as plt
 import random
 
+#Inicializando variáveis globais.
 toggle = True
 G = nx.Graph()
 H = {}
 j = 0
 
-# Função auxiliar para encontrar o índice do conjunto em que o vértice se
-# encontra
-def encontrar_conjunto(lista, vertice):
+#Inicialmente cada vértice é colocado num grupo distinto e cada vez que uma aresta é
+#inserida, os dois vértices extremidades passam a fazer parte do mesmo grupo. Assim, arestas que
+#ligam vértices do mesmo grupo, formam ciclos e devem ser evitadas.
+
+#Função que retorna o grupo a que o vértice pertence.
+def make_set(lista, vertice):
     i = 0
-    for conjunto in lista:
-        if (vertice in conjunto):
+    for grupo in lista:
+        if (vertice in grupo):
             return i
         i = i+1
 
+#Função que realiza o algoritmo de Kruskal no grafo passado por parâmetro.
 def Kruskal(G = nx.Graph()):
     i = 0
     
     MST = nx.create_empty_copy(G)
-
-    E = sorted(G.edges(data=True), key=lambda k: k[2]['weight'])
+    
+    #Ordenando as arestas por pesos.
+    E = sorted(G.edges(data=True), key=lambda k: k[2]['weight']) 
    
-    vertices_conexos = []
+    vertices = []
 
-    # criamos uma lista de conjuntos disjuntos com apenas um vértice cada um, a
-    # princípio, para depois fazermos as uniões
+    #Cria uma lista de grupos distintos com apenas um vértice cada, inicialmente.
     for v in G.nodes():
-        vertices_conexos.append({v})
+        vertices.append({v})
 
+    #Para cada aresta em E, cria dois grupos contendo vertices distintos.
+    #Vértices extremidades de uma mesma aresta fazem parte do mesmo grupo.
     for aresta in E:
-        indexConj1 = encontrar_conjunto(vertices_conexos, aresta[0])
-        indexConj2 = encontrar_conjunto(vertices_conexos, aresta[1])
+        Grupo1 = make_set(vertices, aresta[0])
+        Grupo2 = make_set(vertices, aresta[1])
 
-        # Se o conjunto encontrado para o vértice 0 é o mesmo do vértice 1,
-        # então não podemos uni-los, já que isto fecharia um ciclo.
-        if indexConj1 != indexConj2:
-
-            # Se o grafo contém o peso, então adicionamos as três informações
-            # da aresta (2 vértices e dados)
+        #Se os grupos dos vértices forem iguais não podemos uni-los pois isso fecharia um ciclo.
+        #Se os grupos forem diferentes,
+        if Grupo1 != Grupo2:
+            
+            #Adicionamos as arestas ao novo grafo formado.
             peso = aresta[2]['weight']
             MST.add_edge(aresta[0], aresta[1], weight = peso)
             H[i] = MST.copy()
             i = i + 1
 
-            # removemos os dois conjuntos de vértices do vetor vertices_conexos
-            if indexConj1 > indexConj2:
-                conj1 = vertices_conexos.pop(indexConj1)
-                conj2 = vertices_conexos.pop(indexConj2)
+            #Removendo os dois grupos de vértices do vetor "vertices".
+            if Grupo1 > Grupo2:
+                conj1 = vertices.pop(Grupo1)
+                conj2 = vertices.pop(Grupo2)
             else:
-                conj2 = vertices_conexos.pop(indexConj2)
-                conj1 = vertices_conexos.pop(indexConj1)
+                conj2 = vertices.pop(Grupo2)
+                conj1 = vertices.pop(Grupo1)
 
-            # e inserimos um novo conjunto a partir da união dos dois
-            vertices_conexos.append(conj1.union(conj2))
-
+            #Inserindo um novo grupo a partir da união dos dois.
+            vertices.append(conj1.union(conj2))
+            
+#Função onClick que define o clique na tela do programa.
 def onclick(event):
     global toggle
     global j
    
-    event.canvas.figure.clear()
+    event.canvas.figure.
     
+    #Se foi identificado o clique, imprime o grafo inicial na tela
     if toggle:
         labels = {}
         for v1,v2,data in G.edges(data=True):
@@ -73,6 +83,7 @@ def onclick(event):
         nx.draw_networkx_edge_labels(G, pos, labels)
         toggle = not toggle
 
+    #Senão, imprime o passo a passo do novo grafo encontrado.
     else:
         labels = {}
         for v1,v2,data in H[j].edges(data=True):
@@ -83,13 +94,19 @@ def onclick(event):
 
     event.canvas.draw()
 
+#Main
+    
+#Cria um grafo aleatório com 10 vértices e 15 arestas.
 G = nx.gnm_random_graph(10, 15)
 
+#Da pesos aleatórios de 1 a 10 às arestas do grafo.
 for (u, v) in G.edges():
     G[u][v]['weight'] = random.randint(1,10)
-    
+
+#Chama a função Kruskal
 Kruskal(G)
 
+#Imprime as imagens na tela.
 pos = nx.spring_layout(G)
 fig = plt.figure()
 fig.canvas.mpl_connect('button_press_event', onclick)
--- Prim.py
+++ Prim.py
@@ -1,85 +1,105 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+#Importando as bibliotexas do Python.
 import networkx as nx
 import numpy as n
 import matplotlib.pyplot as plt
+import random
 
+#Inicializando variáveis globais.
 toggle = True
 G = nx.Graph()
 H = {}
 j = 0
 
+#Função que realiza o algoritmo de Prim no grafo passado por parâmetro.
 def Prim(G = nx.Graph(), R = None):
     i = 0
     Q  = {}
-    pred = {}
+    predecessor = {}
 
+    #Para todos os vértices do grafo, inicializamos seus pesos como infinito e seus predecessores como nulos.
     for v,data in G.nodes(data=True):
         Q[v] = n.inf
-        pred[v] = 'null'
-
+        predecessor[v] = 'null'
+        
+    #Para todas as arestas do grafo, se não houver um peso definido seta-o como 1.
     for e,x in G.edges():
         if ('weight' not in G[e][x]):
             G[e][x]['weight'] = 1.0
 
-    Q[R] = 0.0
-    MST  = nx.create_empty_copy(G)
+    Q[R] = 0.0 #Raiz inicia com custo 0 pois é a primeira a sair da fila.
+    MST  = nx.create_empty_copy(G) #Cria uma cópia do grafo inicial.
 
+    #Enquanto houver elementos na fila Q:
     while Q:
-        u = min(Q,key = Q.get)
+        
+        #Remove da fila o vértice de menor prioridade (peso).
+        u = min(Q,key = Q.get) 
         del Q[u]
 
+        #Para todo v vizinho de u
         for vizinho in G[u]:
             if vizinho in Q:
+                #Se o peso da aresta de u até seu vizinho for menor do que o peso de v então
                 if G[u][vizinho]['weight'] < Q[vizinho]:
-                    pred[vizinho] = u
-                    Q[vizinho] = G[u][vizinho]['weight']
+                    predecessor[vizinho] = u #Muda o predecessor de vizinho para u já que encontramos uma entrada melhor.
+                    Q[vizinho] = G[u][vizinho]['weight'] #Peso de vizinho recebe o peso da aresta de u até vizinho, atualizando o custo para o menor valor.
 
-        if pred[u] is not 'null':
-            for v1,v2,data in G.edges(data=True):
-                if (v1 is pred[u] and v2 is u):
-                    MST.add_edge(v1,v2, weight=data['weight'])
+        if predecessor[u] is not 'null':
+            #Para a aresta formada por v1 e v2 com valor "peso"
+            for v1,v2,peso in G.edges(peso=True):
+                #Se v1 é predecessor de v2
+                if (v1 is predecessor[u] and v2 is u):
+                    MST.add_edge(v1,v2, weight=peso['weight']) #Adiciona a aresta formada por esses vértices assim como seu peso no novo grafo MST
+                    H[i] = MST.copy() #Cria uma cópia do grafo MST para H que é um "vetor" de grafos para mostrar o passo a passo da função.
+                    i = i + 1
+                elif ((v1 is u and v2 is predecessor[u]) and (not nx.is_directed(G))): #Se acontecer ao cotrário.
+                    MST.add_edge(v2,v1, weight=peso['weight'])
                     H[i] = MST.copy()
                     i = i + 1
-                elif ((v1 is u and v2 is pred[u]) and (not nx.is_directed(G))):
-                    MST.add_edge(v2,v1, weight=data['weight'])
-                    H[i] = MST.copy()
-                    i = i + 1
-
+                    
+#Função onClick que define o clique na tela do programa.
 def onclick(event):
     global toggle
     global j
    
     event.canvas.figure.clear()
-    
+
+    #Se foi identificado o clique, imprime o grafo inicial na tela
     if toggle:
         labels = {}
-        for v1,v2,data in G.edges(data=True):
-            labels[(v1,v2)] = data['weight']
-        nx.draw(G, pos, with_labels=True)
-        nx.draw_networkx_edge_labels(G, pos, labels)
+        for v1,v2,data in G.edges(peso=True):
+            labels[(v1,v2)] = peso['weight']
+        nx.draw(G, posicao, with_labels=True)
+        nx.draw_networkx_edge_labels(G, posicao, labels)
         toggle = not toggle
-
+        
+    #Senão, imprime o passo a passo do novo grafo encontrado.
     else:
         labels = {}
-        for v1,v2,data in H[j].edges(data=True):
-            labels[(v1,v2)] = data['weight']
-        nx.draw(H[j], pos, with_labels=True)
-        nx.draw_networkx_edge_labels(H[j], pos, labels)
+        for v1,v2,data in H[j].edges(peso=True):
+            labels[(v1,v2)] = peso['weight']
+        nx.draw(H[j], posicao, with_labels=True)
+        nx.draw_networkx_edge_labels(H[j], posicao, labels)
         j = j + 1
 
     event.canvas.draw()
 
+#Main
+    
+#Cria um grafo aleatório com 10 vértices e 15 arestas.
 G = nx.gnm_random_graph(10, 15)
 
-import random
-#code creating G here
+#Da pesos aleatórios de 1 a 10 às arestas do grafo.
 for (u, v) in G.edges():
     G[u][v]['weight'] = random.randint(1,10)
 
+#Chama a função Prim.
 Prim(G, 0)
 
+#Imprime as imagens na tela.
 pos = nx.spring_layout(G)
 fig = plt.figure()
 fig.canvas.mpl_connect('button_press_event', onclick)
